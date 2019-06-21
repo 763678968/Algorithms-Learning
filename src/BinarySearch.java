0bc73557fef52712c4e5f8bd795e1283f614f3b4
Animation <763678968@qq.com>
2019-06-21 16:18:00 +0800

    BinarySearch_3

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -6,7 +6,7 @@ public class BinarySearch {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi) {
-            // 被查找的键要么不存在，要么必然存在于a[lo..hi]之中
+            // 被查找的键要么不存在，要-么必然存在于a[lo..hi]之中
             int mid = lo + (hi - lo) / 2;
             if      (key < a[mid]) hi = mid - 1;
             else if (key > a[mid]) lo = mid + 1;
