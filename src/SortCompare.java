d2d457e5bd0770097ce5f0a5b106add7da5d5c03
Animation <763678968@qq.com>
2019-06-29 09:46:12 +0800

    SortCompare_modified

--- src/SortCompare.java
+++ src/SortCompare.java
@@ -1,14 +1,16 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SortCompare {
     public static double time(String alg, Comparable[] a) {
         Stopwatch timer = new Stopwatch();
         if (alg.equals("Insertion")) Insertion.sort(a);
         if (alg.equals("Selection")) Selection.sort(a);
-        if (alg.equals("Shell"))     Shell.sort(a);
-        if (alg.equals("Merge"))     Merge.sort(a);
-        if (alg.equals("Quick"))     Quick.sort(a);
-        if (alg.equals("Heap"))      Heap.sort(a);
+//        if (alg.equals("Shell"))     Shell.sort(a);
+//        if (alg.equals("Merge"))     Merge.sort(a);
+//        if (alg.equals("Quick"))     Quick.sort(a);
+//        if (alg.equals("Heap"))      Heap.sort(a);
         return timer.elapsedTime();
     }
 
@@ -32,6 +34,6 @@ public class SortCompare {
         double t1 = timeRandomInput(alg1, N, T);
         double t2 = timeRandomInput(alg2, N, T);
         StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
-        StdOut.printf(" %.1f times faster than %\n", t2/t1, alg2);
+        StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
     }
 }
