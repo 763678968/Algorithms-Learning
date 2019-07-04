759f9d98ab1f1eb66355c911cd504e05dba69bee
animation <763678968@qq.com>
2019-07-04 15:13:57 +0800

    VectorDemo1

---
+++ src/VectorDemo1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class VectorDemo1 {
+    public static void main(String[] args) {
+        Vector<Integer> vec = new Vector<Integer>();
+        int rdm = 0;
+        for (int i = 0; i < 20; i++) {
+            rdm = (int)(1 + Math.random()*100);
+            vec.add(rdm);
+        }
+        for (int i : vec)
+            System.out.print(i + " ");
+    }
+}
