b3c5511fc1668c37d4846060cca1ae79ec80f18d
animation <763678968@qq.com>
2019-07-04 15:45:43 +0800

    VectorDemo_2

--- src/VectorDemo.java
+++ src/VectorDemo.java
@@ -8,6 +8,11 @@ public class VectorDemo {
             rdm = (int)(1 + Math.random()*100);
             vec.add(rdm);
         }
+        print_vector(vec);
+    }
+
+    public static void print_vector(Vector<Integer> vec) {
+        System.out.print("数组大小：" + vec.size() + ", 数组元素：");
         for (int i : vec)
             System.out.print(i + " ");
     }
