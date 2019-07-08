68b14dd61ffd94d6e0a801cf3857228945da229d
animation <763678968@qq.com>
2019-07-08 10:48:41 +0800

    FrequencyCounter_modified

--- src/FrequencyCounter.java
+++ src/FrequencyCounter.java
@@ -6,17 +6,15 @@ public class FrequencyCounter {
 
     public static void main(String[] args) {
 
-        int minlen = Integer.parseInt(args[0]); // 最小键长
+        int minlen = Integer.parseInt(args[0]);
         ST<String, Integer> st = new ST<String, Integer>();
         while (!StdIn.isEmpty()) {
-            // 构造符号表并统计频率
+
             String word = StdIn.readString();
-            if (word.length() < minlen) continue; // 忽略较短的单词
+            if (word.length() < minlen) continue;
             if (!st.contains(word)) st.put(word, 1);
             else                    st.put(word, st.get(word) + 1);
         }
-
-        // 找出出现频率最高的单词
 
         String max = " ";
         st.put(max, 0);
