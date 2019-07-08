659a73f4f2e8a9a98da1c55d2cab2ae45df6a6e3
animation <763678968@qq.com>
2019-07-08 16:39:08 +0800

    BST_get/put

--- src/BST.java
+++ src/BST.java
@@ -27,15 +27,38 @@ public class BST<Key extends Comparable<Key>, Value> {
     }
 
     public Value get(Key key) {
-        // 请见算法3.3（续1）
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        // 在以x为根结点的子树中查找并返回key所对应的值；
+        // 如果找不到则返回null
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)        return get(x.left, key);
+        else if (cmp > 0)   return get(x.right, key);
+        else                return x.val;
     }
 
     public void put(Key key, Value val) {
-        // 请见算法3.3（续1）
+        // 查找key，找到则更新它的值，否则为它创建一个新的结点
+        root = put(root, key, val);
 
         // max()、min()、floor()、ceiling()方法请见算法3.3（续2）
         // select()、rank()方法请见算法3.3（续3）
         // delete()、deleteMin()、deleteMax()方法请见算法3.3（续4）
         // keys()方法请见算法3.3（续5）
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        // 如果key存在于以x为根结点的子树中则更新它的值；
+        // 否则将以key和val为键值对的新结点插入到该子树中
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
     }
 }
