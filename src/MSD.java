32b6002c8aee9645d5ec3ad353ef08a57dc0b08a
animation <763678968@qq.com>
2019-07-12 22:33:33 +0800

    MSD_modified

--- src/MSD.java
+++ src/MSD.java
@@ -1,7 +1,7 @@
 public class MSD {
-    private static int R = 256;
-    private static final int M = 15;
-    private static String[] aux;
+    private static int R = 256;         // 基数
+    private static final int M = 15;    // 小数组的切换阈值
+    private static String[] aux;        // 数据分类的辅助数组
 
     private static int charAt(String s, int d) {
         if (d < s.length())
@@ -19,21 +19,21 @@ public class MSD {
     private static void sort(String[] a, int lo, int hi, int d) {
         // 以第d个字符为键将a[lo]至a[hi]排序
         if (hi <= lo + M) {
-            Insertion.sort(a, lo, hi, d);
+            Insertion.sort(a);
             return;
         }
 
-        int[] count = new int[R+2];
+        int[] count = new int[R+2];         // 计算频率
         for (int i = lo; i <= hi; i++) {
             count[charAt(a[i], d) + 2]++;
         }
-        for (int r = 0; r < R+1; r++) {
+        for (int r = 0; r < R+1; r++) {     // 将频率转换为索引
             count[r+1] += count[r];
         }
-        for (int i = lo; i <= hi; i++) {
+        for (int i = lo; i <= hi; i++) {    // 数据分类
             aux[count[charAt(a[i], d) + 1]++] = a[i];
         }
-        for (int i = lo; i <= hi; i++) {
+        for (int i = lo; i <= hi; i++) {    // 回写
             a[i] = aux[i - lo];
         }
 
