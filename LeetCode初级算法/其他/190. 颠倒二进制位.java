74640dfe40cac860b6af6f0c8893897d6bc4d826
Qi <36908325+763678968@users.noreply.github.com>
2019-07-16 21:52:26 +0800

    Create 190. 颠倒二进制位.java

---
+++ LeetCode初级算法/其他/190. 颠倒二进制位.java
@@ -0,0 +1,28 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;   // CATCH: must do unsigned shift
+            if (i < 31) // CATCH: for last digit, don't shift!
+                result <<= 1;
+        }
+        return result;
+    }
+}
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if (n == 0) return 0;
+        
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            if ((n & 1) == 1) result++;
+            n >>= 1;
+        }
+        return result;
+    }
+}
