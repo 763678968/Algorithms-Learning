70252bdedd502a3d5df13db14642fd91f5194915
Qi <36908325+763678968@users.noreply.github.com>
2019-07-16 21:52:44 +0800

    Create 118. 帕斯卡三角形.java

---
+++ LeetCode初级算法/其他/118. 帕斯卡三角形.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size()-1; j++) {
+                row.set(j, row.get(j)+row.get(j+1));
+            }
+            allrows.add(new ArrayList<Integer>(row));
+        }
+        return allrows;
+    }
+}
+// 较慢
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        if (numRows <= 0) {
+            return triangle;
+        }
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j < i + 1; j++) {
+                if (j==0 || j==i) {
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
+// 较快
