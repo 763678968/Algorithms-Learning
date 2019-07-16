0c0ee4dc551f22af68cc20f507cbc6e7fbd285b2
Qi <36908325+763678968@users.noreply.github.com>
2019-07-16 21:53:02 +0800

    Create 20. 有效的括号.java

---
+++ LeetCode初级算法/其他/20. 有效的括号.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
+// 使用字符串的toCharArray()方法，使用栈，isEmpty()、pop()方法
