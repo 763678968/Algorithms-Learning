8923b4decc53627eed1a73a6609c0a614b7ac39e
animation <763678968@qq.com>
2019-10-29 23:12:39 +0800

    3.找出数组中重复的数字

---
+++ 剑指Offer/3. 找出数组中重复的数字.java
@@ -0,0 +1,32 @@
+public class FindDuplication {
+
+    /**
+     * 找到数组中一个重复的数字
+     * 返回-1代表无重复数字或者输入无效
+     */
+    public int getDuplicate(int[] arr) {
+        if (arr == null || arr.length <= 0) {
+            System.out.println("数组输入无效！");
+            return -1;
+        }
+        for (int a : arr) {
+            if (a < 0 || a > arr.length - 1) {
+            }
+                System.out.println("数字大小超出范围！");
+                return -1;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int temp;
+            while (arr[i] != i) {
+                if (arr[arr[i]] == arr[i])
+                    return arr[i];
+                // 交换arr[arr[i]]和arr[i]
+                temp = arr[i];
+                arr[i] = arr[temp];
+                arr[temp] = temp;
+            }
+        }
+        System.out.println("数组中无重复数字！");
+        return -1;
+    }
