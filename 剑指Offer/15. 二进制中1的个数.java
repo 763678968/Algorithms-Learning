f63efb9ded0ecc837bce23c57704a7e711fb3f22
Qi <36908325+763678968@users.noreply.github.com>
2019-11-03 22:48:30 +0800

    Update 15. 二进制中1的个数.java

--- 剑指Offer/15. 二进制中1的个数.java
+++ 剑指Offer/15. 二进制中1的个数.java
@@ -18,5 +18,25 @@ public class Solution {
     }
     
     // 常规解法
+    public int NumberOf1(int n) {
+        int count = 0;
+        int flag = 1;
+
+        while (flag != 0) {
+            if ((n & flag) != 0)
+                count++;
+            flag = flag << 1;
+        }
+        return count;
+    }
     
+    // 最优解，位与运算
+    public int NumberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = (n - 1) & n;
+        }
+        return count;
+    }
 }
