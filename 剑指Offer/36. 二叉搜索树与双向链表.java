ab7676f66569cb6c97e148f95f3ba5028993a2d0
Qi <36908325+763678968@users.noreply.github.com>
2019-11-08 22:33:11 +0800

    Update 36. 二叉搜索树与双向链表.java

--- 剑指Offer/36. 二叉搜索树与双向链表.java
+++ 剑指Offer/36. 二叉搜索树与双向链表.java
@@ -1,3 +1,4 @@
+// 递归实现
 public class Solution {
     public TreeNode Convert(TreeNode pRootOfTree) {
         if (pRootOfTree == null)
@@ -29,4 +30,33 @@ public class Solution {
         }
         return left != null ? left : pRootOfTree;
     }
+}
+
+// 非递归实现
+import java.util.Stack;
+public TreeNode ConvertBSTToBiList(TreeNode root) {
+    if(root==null)
+        return null;
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    TreeNode p = root;
+    TreeNode pre = null;// 用于保存中序遍历序列的上一节点
+    boolean isFirst = true;
+    while(p!=null||!stack.isEmpty()){
+        while(p!=null){
+            stack.push(p);
+            p = p.left;
+        }
+        p = stack.pop();
+        if(isFirst){
+            root = p;// 将中序遍历序列中的第一个节点记为root
+            pre = root;
+            isFirst = false;
+        }else{
+            pre.right = p;
+            p.left = pre;
+            pre = p;
+        }      
+        p = p.right;
+    }
+    return root;
 }
