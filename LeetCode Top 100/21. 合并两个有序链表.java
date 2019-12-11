c802ee62ec90f08ef6c31fc471d5d6b93577e8eb
Qi <36908325+763678968@users.noreply.github.com>
2019-12-11 21:37:42 +0800

    Create 21. 合并两个有序链表.java

---
+++ LeetCode Top 100/21. 合并两个有序链表.java
@@ -0,0 +1,39 @@
+// 迭代做法
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        // 当其中一个链表已经合并完成后，直接将另一个链表的剩余部分接续上即可
+        cur.next = l1 == null ? l2 : l1;
+        return res.next;
+    }
+}
+
+// 递归做法
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
