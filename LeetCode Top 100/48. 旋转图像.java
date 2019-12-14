81c3d17e44e0825c1118142c879c24ea775e1e54
Qi <36908325+763678968@users.noreply.github.com>
2019-12-14 23:18:59 +0800

    Create 48. 旋转图像.java

---
+++ LeetCode Top 100/48. 旋转图像.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int start = 0, end = matrix.length - 1;
+        while (start < end) {
+            int[] temp = matrix[start];
+            matrix[start] = matrix[end];
+            matrix[end] = temp;
+            start++; end--;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
