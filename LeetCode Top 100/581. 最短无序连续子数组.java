9a5105e2986aa11c283dfba3aaa770d339db47ec
Qi <36908325+763678968@users.noreply.github.com>
2019-12-24 21:24:50 +0800

    Create 581. 最短无序连续子数组.java

---
+++ LeetCode Top 100/581. 最短无序连续子数组.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int start = -1, end = -2;
+        int max = nums[0], min = nums[n - 1];
+
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n - 1 - i]);
+            if (nums[i] < max) end = i;
+            if (nums[n - 1 - i] > min) start = n - 1 - i;
+        }
+        return end - start + 1;
+    }
+}
