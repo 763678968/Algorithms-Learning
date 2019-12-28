7086742a44e5a15ccada78ad7b6dc95f3e2c5950
Qi <36908325+763678968@users.noreply.github.com>
2019-12-28 23:27:45 +0800

    Create 145. 二叉树的后序遍历.java

---
+++ LeetCode分类题解/树/145. 二叉树的后序遍历.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        /**
+         * 前序遍历输出顺序为root → left → right
+         * 后序遍历输出顺序为left → right → root
+         * 将后序遍历倒序，则变为root → right → left
+         * 倒序后的序列，与前序遍历相比，只需要将left、right入栈顺序交换即可
+         */
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node == null) continue;
+            res.add(node.val);
+            // 因此这里先将left入栈，保证right先出栈
+            stack.push(node.left);
+            stack.push(node.right);
+        }
+        // 将序列从root → right → left反转为left → right → root
+        Collections.reverse(res);
+        return res;
+    }
+}
