6596cfdcd842210b7da0d049c57ae7956797c733
animation <763678968@qq.com>
2020-01-08 21:44:33 +0800

    链表每K个节点进行反转

--- 高频算法题/链表/链表每K个元素进行反转.java
+++ 高频算法题/链表/链表每K个元素进行反转.java
@@ -1,3 +1,29 @@
+// 非递归方法
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int n = 0;
+        // 计算链表的长度
+        for (ListNode i = head; i != null; n++, i = i.next);
+
+        ListNode start = new ListNode(0);
+        start.next = head;
+        for(ListNode prev = start, tail = head; n >= k; n -= k) {
+            for (int i = 1; i < k; i++) {
+                ListNode next = tail.next.next;
+                tail.next.next = prev.next;
+                prev.next = tail.next;
+                tail.next = next;
+            }
+
+            prev = tail;
+            tail = tail.next;
+        }
+        return start.next;
+    }
+}
+
+
+// 递归方法，内存超限
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         /**
