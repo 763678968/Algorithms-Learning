ffec5ff4bd5a5fc6d591f840b8ecf38dcd45fea3
animation <763678968@qq.com>
2020-01-09 21:06:09 +0800

    二叉树的前序遍历 递归、迭代

---
+++ 高频算法题/二叉树/二叉树的前序遍历.java
@@ -0,0 +1,30 @@
+// 递归
+class Solution {
+    public void preOrder(TreeNode root) {
+        if (root != null) {
+            // 中、左、右
+            System.out.println(root.val + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+}
+
+
+// 非递归
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node == null) continue;
+            res.add(node.val);
+            // 先将右子树入栈，保证出栈时左子树先遍历
+            stack.push(node.right);
+            stack.push(node.left);
+        }
+        return res;
+    }
+}
