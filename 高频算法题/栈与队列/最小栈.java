bb0e978fe0d901cd4588ec16952fe4e7ab0d3298
animation <763678968@qq.com>
2020-01-11 22:04:31 +0800

    最小栈

---
+++ 高频算法题/栈与队列/最小栈.java
@@ -0,0 +1,33 @@
+class MinStack {
+    private Stack<Integer> dataStack;
+    private Stack<Integer> minStack;
+    private int min;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        dataStack = new Stack<>();
+        minStack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+
+    public void push(int x) {
+        dataStack.add(x);
+        min = Math.min(min, x);
+        minStack.add(min);
+    }
+
+    public void pop() {
+        dataStack.pop();
+        minStack.pop();
+        // minStack为空时，将min更新为默认值
+        min = minStack.isEmpty() ? Integer.MAX_VALUE : minStack.peek();
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
