b2ab85fb0d5fb43605aff1a03eb64d455dd7ebf7
animation <763678968@qq.com>
2020-01-11 09:49:09 +0800

    二叉树的镜像 迭代

--- 高频算法题/二叉树/二叉树的镜像.java
+++ 高频算法题/二叉树/二叉树的镜像.java
@@ -1,3 +1,4 @@
+// 递归
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
@@ -5,6 +6,32 @@ class Solution {
         TreeNode left = root.left, right = root.right;
         root.left = invertTree(right);
         root.right = invertTree(left);
+        return root;
+    }
+}
+
+// 迭代
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null || node.right != null) {
+                TreeNode temp = node.left;
+                node.left = node.right;
+                node.right = temp;
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
         return root;
     }
 }
