96527e8ed7a5ab8b57c0b8ec3caf524f6f3e680c
animation <763678968@qq.com>
2020-01-14 19:20:43 +0800

    选择排序

---
+++ 高频算法题/排序算法/选择排序.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static void selectionSort(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                // 从未排序的序列中找到最小的数
+                if (array[j] < array[minIndex]) {
+                    // 将最小数的索引保存
+                    minIndex = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2,4,5,1,4,5,6,8,3};
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
