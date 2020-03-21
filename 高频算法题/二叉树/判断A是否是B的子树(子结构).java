45db660089bb6659fa767e92e184c5a6fe273be1
animation <763678968@qq.com>
2020-03-22 00:10:34 +0800

    统一格式

--- 高频算法题/二叉树/判断A是否是B的子树(子结构).java
+++ 高频算法题/二叉树/判断A是否是B的子树(子结构).java
@@ -40,6 +40,29 @@ class Solution {
     }
 }
 
+// 上面代码的简单写法
+public class Solution {
+    public boolean isSubtree(TreeNode s,TreeNode t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        return isSubtreeWithRoot(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSubtreeWithRoot(TreeNode s, TreeNode t) {
+        if (t == null) {
+            return true;
+        }
+        if (s == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isSubtreeWithRoot(s.left, t.left) && isSubtreeWithRoot(s.right, t.right);
+    }
+}
+
 
 // 子树  LeetCode 572
 class Solution {
