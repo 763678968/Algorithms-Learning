105a440e2da651c64c7bba16d09a0fbb7deb42ba
animation <763678968@qq.com>
2020-03-24 21:57:13 +0800

    单例模式枚举实现

--- 高频算法题/设计/单例模式.java
+++ 高频算法题/设计/单例模式.java
@@ -82,22 +82,22 @@ public enum Singleton {
 
     // 单例测试
     public static void main(String[] args) {
-        Singleton firstSingleton = new Singleton();
+        Singleton firstSingleton = Singleton.INSTANCE;
         firstSingleton.setObjName("firstName");
         System.out.println(firstSingleton.getObjName());
-        Singleton secondSingleton = new Singleton();
+        Singleton secondSingleton = Singleton.INSTANCE;
         secondSingleton.setObjName("secondName");
         System.out.println(firstSingleton.getObjName());
         System.out.println(secondSingleton.getObjName());
-    }
 
-    // 反射获取实例测试
-    try {
-        Singleton[] enumConstants = Singleton.class.getEnumConstants();
-        for (Singleton enumConstant : enumConstants) {
-            System.out.println(enumConstant.getObjName());
+        // 反射获取实例测试
+        try {
+            Singleton[] enumConstants = Singleton.class.getEnumConstants();
+            for (Singleton enumConstant : enumConstants) {
+                System.out.println(enumConstant.getObjName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    } catch (Exception e) {
-        e.printStackTrace();
     }
 }
