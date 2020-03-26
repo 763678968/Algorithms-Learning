4166c2b14d089c23da0f202b14305607c375fba1
animation <763678968@qq.com>
2020-03-27 00:09:12 +0800

    更新N皇后问题的写法

--- 高频算法题/动态规划 贪心算法 递归 回溯/N皇后问题.java
+++ 高频算法题/动态规划 贪心算法 递归 回溯/N皇后问题.java
@@ -1,3 +1,68 @@
+// 一种更简明的写法
+public class Solution {
+    // n皇后问题
+    List<List<String>> res = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (char[] chars : board) {
+            Arrays.fill(chars, '.');
+        }
+        backtrack(board, 0);
+        return res;
+    }
+
+    private void backtrack(char[][] board, int row) {
+        if (row == board.length) {
+            res.add(charToString(board));
+        }
+        int n = board.length;
+        for (int col = 0; col < n; col++) {
+            if (!isValid(board, row, col)) {
+                continue;
+            }
+            board[row][col] = 'Q';
+            backtrack(board, row + 1);
+            board[row][col] = '.';
+        }
+    }
+
+    // 检查当前位置是否可以放置皇后
+    private boolean isValid(char[][] board, int row, int col) {
+        int cols = board.length;
+        // 判断当前列是否存在冲突
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // 判断右上方是否存在冲突
+        for (int i = row - 1, j = col + 1; i >= 0 && j < cols; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // 判断左上方是否存在冲突
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 用来将字符型矩阵转为每行一个String的List
+    private List<String> charToString(char[][] array) {
+        List<String> res = new ArrayList<>();
+        for (char[] chars : array) {
+            res.add(String.valueOf(chars));
+        }
+        return res;
+    }
+}
+
+
 class Solution {
     // 定义一个max表示共有多少个皇后
     int max = 8;
